5cb384a0d3715c47696ed4546d4952df91366fc1
remackgeek <bmack@bluewiz.net>
2020-03-14 13:54:02 -0700

    fix test

--- projects/p-table-responsive-scroll/src/lib/responsive-scroll/responsive-scroll.directive.spec.ts
+++ projects/p-table-responsive-scroll/src/lib/responsive-scroll/responsive-scroll.directive.spec.ts
@@ -1,8 +1,10 @@
 import { ResponsiveScrollDirective } from './responsive-scroll.directive';
+const table = { scrollHeight: 0 };
+const zone = { run: () => {} };
 
 describe('ResponsiveScrollDirective', () => {
   it('should create an instance', () => {
-    const directive = new ResponsiveScrollDirective({ scrollHeight: 0 } as any);
+    const directive = new ResponsiveScrollDirective(table as any, zone as any);
     expect(directive).toBeTruthy();
   });
 });
